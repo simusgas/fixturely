38d78f601362c5f0f4dacbbe3b34a1c7dc49f24c
Simao <simao@Simaos-MacBook-Air.local>
2026-03-23 16:18:13 +0800

    fix chromium for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['playwright-core', '@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/api/courts/book': ['./node_modules/@sparticuz/chromium/bin/**'],
+  },
 };
 
 export default nextConfig;
